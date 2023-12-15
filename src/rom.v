2d77f8a3b6c6ceae880ab4a7764054747bab2eca
JoeDavisson <joe63074@gmail.com>
2023-12-14 18:08:46 -0600

    Make ROM address bus wider

--- src/rom.v
+++ src/rom.v
@@ -11,48 +11,19 @@
 
 module rom
 (
-  input  [5:0] address,
-  output [7:0] data_out
+  input [11:0] address,
+  output reg [31:0] data_out,
+  input clk
 );
 
-reg [7:0] data;
-assign data_out = data;
+reg [31:0] memory [511:0];
 
-always @(address) begin
-  case (address)
-/*
-     // ad 03 40 00
-     // lda 0x4004
-     0: data <= 8'had;
-     1: data <= 8'h04;
-     2: data <= 8'h40;
-     //0: data <= 8'ha9;
-     //1: data <= 8'ha5;
-     // brk
-     3: data <= 8'h00;
-     // .db 0x81
-     4: data <= 8'h81;
-*/
-     // ldx #0xe0
-     0: data <= 8'ha2;
-     1: data <= 8'he0;
-     // stx 0x00   100 001 10
-     2: data <= 8'h86;
-     3: data <= 8'h00;
-     // lda 0x00
-     4: data <= 8'ha5;
-     5: data <= 8'h00;
-     // sec
-     6: data <= 8'h38;
-     // php
-     7: data <= 8'h08;
-     // pla
-     8: data <= 8'h68;
-     // brk
-     9: data <= 8'h00;
+initial begin
+  $readmemh("rom.txt", memory);
+end
 
-    default: data <= 0;
-  endcase
+always @(posedge clk) begin
+  data_out <= memory[address[11:2]];
 end
 
 endmodule
